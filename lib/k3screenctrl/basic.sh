050f9173dc4cb69307c29079b3e3a2503442cb03
monokoo <realstones2012@gmail.com>
2017-07-08 18:21:53 +0800

    update basic.sh

--- lib/k3screenctrl/basic.sh
+++ lib/k3screenctrl/basic.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-. /etc/os-release
+. /etc/openwrt_release
 
 PRODUCT_NAME_FULL=$(cat /etc/board.json | jsonfilter -e "@.model.name")
 PRODUCT_NAME=${PRODUCT_NAME_FULL#* } # Remove first word to save space
@@ -7,8 +7,8 @@ WAN_IFNAME=$(uci get network.wan.ifname)
 MAC_ADDR=$(ifconfig $WAN_IFNAME | grep -oE "([0-9A-Z]{2}:){5}[0-9A-Z]{2}")
 CPU_TEMP=$(($(cat /sys/class/thermal/thermal_zone0/temp) / 1000))
 disp=$(uci get k3screenctrl.@general[0].cputemp)
-HW_VERSION=${LEDE_DEVICE_REVISION:0:3}
-FW_VERSION=${PRETTY_NAME:0:19}
+HW_VERSION=${DISTRIB_CODENAME:1:5}
+FW_VERSION=v$HW_VERSION" "${DISTRIB_RELEASE:0:9}
 
 echo $PRODUCT_NAME
 if [ "$disp" -eq 1 ]; then
