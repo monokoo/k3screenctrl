d1fde3844617b44aa3a7abaf0b60ea9648291660
monokoo <realstones2012@gmail.com>
2017-07-15 19:29:58 +0800

    added uptime

--- lib/k3screenctrl/basic.sh
+++ lib/k3screenctrl/basic.sh
@@ -4,7 +4,11 @@
 PRODUCT_NAME_FULL=$(cat /etc/board.json | jsonfilter -e "@.model.name")
 PRODUCT_NAME=${PRODUCT_NAME_FULL#* } # Remove first word to save space
 WAN_IFNAME=$(uci get network.wan.ifname)
+
 MAC_ADDR=$(ifconfig $WAN_IFNAME | grep -oE "([0-9A-Z]{2}:){5}[0-9A-Z]{2}")
+RUPTIME=$(awk '{print int($1/86400)"days "int($1%86400/3600)"h "int(($1%3600)/60)"m"}' /proc/uptime)
+router_uptime=$(uci get k3screenctrl.@general[0].router_uptime)
+
 CPU_TEMP=$(($(cat /sys/class/thermal/thermal_zone0/temp) / 1000))
 disp=$(uci get k3screenctrl.@general[0].cputemp)
 HW_VERSION=${DISTRIB_CODENAME:1:5}
@@ -17,4 +21,8 @@ else
 	echo $HW_VERSION
 fi
 echo $FW_VERSION
-echo $MAC_ADDR
+if [ "$router_uptime" -eq 1 ]; then
+	echo $MAC_ADDR
+else
+	echo $RUPTIME
+fi
