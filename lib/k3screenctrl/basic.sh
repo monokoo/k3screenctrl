81a4410272b4fdf8b0bcfbcdc651dd0457d981fc
monokoo <realstones2012@gmail.com>
2017-09-13 11:51:13 +0800

    format basic.sh

--- lib/k3screenctrl/basic.sh
+++ lib/k3screenctrl/basic.sh
@@ -3,33 +3,33 @@
 
 PRODUCT_NAME_FULL=$(cat /etc/board.json | jsonfilter -e "@.model.name")
 PRODUCT_NAME=${PRODUCT_NAME_FULL#* } # Remove first word to save space
-WAN_IFNAME=$(uci get network.wan.ifname)
-
-LAN_ADDR=`ifconfig br-lan |grep -w "inet addr"| awk '{print $2}'|awk -F':' '{print $2}' 2>/dev/null`
-[ -z "$LAN_ADDR" ] && LAN_ADDR=`uci get network.lan.ipaddr 2>/dev/null`
-[ -z "$LAN_ADDR" ] && LAN_ADDR="p.to"
-MAC_ADDR=$(ifconfig $WAN_IFNAME 2>/dev/null | grep -oE "([0-9A-Z]{2}:){5}[0-9A-Z]{2}")
-[ -z "$MAC_ADDR" ] && MAC_ADDR=$(ifconfig eth0 2>/dev/null | grep -oE "([0-9A-Z]{2}:){5}[0-9A-Z]{2}")
-RUPTIME=$(awk '{print int($1/86400)"days "int($1%86400/3600)"h "int(($1%3600)/60)"m"}' /proc/uptime)
 router_uptime=$(uci get k3screenctrl.@general[0].router_uptime)
-
-CPU_TEMP=$(awk 'BEGIN{printf "%.2f\n",'$(cat /sys/class/thermal/thermal_zone0/temp)'/1000}')"'C"
 disp=$(uci get k3screenctrl.@general[0].cputemp)
 
-HW_VERSION=$(cat /etc/os-release | grep -w "PRETTY_NAME" | awk '{print $2}')
-FW_VERSION=$HW_VERSION" "${DISTRIB_RELEASE:0:9}
-
 echo $PRODUCT_NAME
+
 if [ "$disp" -eq 1 ]; then
+	CPU_TEMP=$(awk 'BEGIN{printf "%.2f\n",'$(cat /sys/class/thermal/thermal_zone0/temp)'/1000}')"'C"
 	echo $CPU_TEMP
 else
+	HW_VERSION=$(cat /etc/os-release | grep -w "PRETTY_NAME" | awk '{print $2}')
+	FW_VERSION=$HW_VERSION" "${DISTRIB_RELEASE:0:9}
 	echo $HW_VERSION
 fi
+
 echo $FW_VERSION
+
 if [ "$router_uptime" -eq 0 ]; then
+	LAN_ADDR=`ifconfig br-lan |grep -w "inet addr"| awk '{print $2}'|awk -F':' '{print $2}' 2>/dev/null`
+	[ -z "$LAN_ADDR" ] && LAN_ADDR=`uci get network.lan.ipaddr 2>/dev/null`
+	[ -z "$LAN_ADDR" ] && LAN_ADDR="p.to"
 	echo $LAN_ADDR
 elif [ "$router_uptime" -eq 1 ]; then
+	RUPTIME=$(awk '{print int($1/86400)"days "int($1%86400/3600)"h "int(($1%3600)/60)"m"}' /proc/uptime)
 	echo $RUPTIME
 else
+	WAN_IFNAME=$(uci get network.wan.ifname)
+	MAC_ADDR=$(ifconfig $WAN_IFNAME 2>/dev/null | grep -oE "([0-9A-Z]{2}:){5}[0-9A-Z]{2}")
+	[ -z "$MAC_ADDR" ] && MAC_ADDR=$(ifconfig eth0 2>/dev/null | grep -oE "([0-9A-Z]{2}:){5}[0-9A-Z]{2}")
 	echo $MAC_ADDR
 fi
