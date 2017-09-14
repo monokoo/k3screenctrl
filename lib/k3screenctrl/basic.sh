09987328949201bbd181de92b9af2553a6bcd983
monokoo <realstones2012@gmail.com>
2017-09-14 09:47:47 +0800

    fix basic fw version issue by last commit

--- lib/k3screenctrl/basic.sh
+++ lib/k3screenctrl/basic.sh
@@ -5,6 +5,8 @@ PRODUCT_NAME_FULL=$(cat /etc/board.json | jsonfilter -e "@.model.name")
 PRODUCT_NAME=${PRODUCT_NAME_FULL#* } # Remove first word to save space
 router_uptime=$(uci get k3screenctrl.@general[0].router_uptime)
 disp=$(uci get k3screenctrl.@general[0].cputemp)
+HW_VERSION=$(cat /etc/os-release | grep -w "PRETTY_NAME" | awk '{print $2}')
+FW_VERSION=$HW_VERSION" "${DISTRIB_RELEASE:0:9}
 
 echo $PRODUCT_NAME
 
@@ -12,8 +14,6 @@ if [ "$disp" -eq 1 ]; then
 	CPU_TEMP=$(awk 'BEGIN{printf "%.2f\n",'$(cat /sys/class/thermal/thermal_zone0/temp)'/1000}')"'C"
 	echo $CPU_TEMP
 else
-	HW_VERSION=$(cat /etc/os-release | grep -w "PRETTY_NAME" | awk '{print $2}')
-	FW_VERSION=$HW_VERSION" "${DISTRIB_RELEASE:0:9}
 	echo $HW_VERSION
 fi
 
